37334298d40ba835fd572ea44dfa6818b25daf1c
Tran Thanh Dat <tthanhdat.pb@gmail.com>
2024-09-29 17:20:05 +0700

    create admin service

---
+++ Library/src/main/java/com/thanhdatpb/library/service/impl/AdminServiceImpl.java
@@ -0,0 +1,37 @@
+package com.thanhdatpb.library.service.impl;
+
+import com.thanhdatpb.library.dto.AdminDto;
+import com.thanhdatpb.library.model.Admin;
+import com.thanhdatpb.library.model.Role;
+import com.thanhdatpb.library.repository.AdminRepository;
+import com.thanhdatpb.library.repository.RoleRepository;
+import com.thanhdatpb.library.service.AdminService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
+
+@Service
+public class AdminServiceImpl implements AdminService {
+    @Autowired
+    private AdminRepository adminRepository;
+
+    @Autowired
+    private RoleRepository roleRepository;
+
+    @Override
+    public Admin finByUsername(String username) {
+        return adminRepository.findByUsername(username);
+    }
+
+    @Override
+    public Admin save(AdminDto adminDto) {
+        Admin admin = new Admin();
+        admin.setFirstName(adminDto.getFirstName());
+        admin.setLastName(adminDto.getLastName());
+        admin.setUsername(adminDto.getUsername());
+        admin.setPassword(adminDto.getPassword());
+        admin.setRoles(Arrays.asList(roleRepository.findByName("ADMIN")));
+        return adminRepository.save(admin);
+    }
+}
